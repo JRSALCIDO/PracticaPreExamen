68ec99734d8af85b93205ae5cf0fd52288d916ae
JRSALCIDO <salcido.12311@gmail.com>
2023-05-28 15:14:24 -0700

    Se codifico el mainActivity

--- app/src/main/java/com/example/practicapreexamen/MainActivity.kt
+++ app/src/main/java/com/example/practicapreexamen/MainActivity.kt
@@ -1,13 +1,38 @@
 package com.example.practicapreexamen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var entrarButton: Button
+    private lateinit var salirButton: Button
+    private lateinit var totalEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        totalEditText = findViewById(R.id.totalEditText)
+        entrarButton = findViewById(R.id.entrarButton)
+        salirButton = findViewById(R.id.salirButton)
 
+        entrarButton.setOnClickListener {
+            if (totalEditText.text.toString().isNotEmpty()) {
+                val intent = Intent(this, ReciboActivity::class.java)
+                intent.putExtra("nombre", totalEditText.text.toString())
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Por favor ingrese el nombre del trabajador", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        salirButton.setOnClickListener {
+            finish()
+        }
     }
 }
